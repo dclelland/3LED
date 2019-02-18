5e26ea0dfd753cb3b589a92c0bd9b816fadc9c35
Daniel Clelland <daniel.clelland@gmail.com>
2019-02-18 21:01:11 +1300

    Cleaned up project structure

--- 3LED/Controllers/ViewController.swift
+++ 3LED/Controllers/ViewController.swift
@@ -10,11 +10,4 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-
 }
-
