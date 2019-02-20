3adef1a3a05fa4b275e44160ff2c2dc6983445bd
Daniel Clelland <daniel.clelland@gmail.com>
2019-02-20 23:10:01 +1300

    Moved business logic into the app delegate

--- 3LED/AppDelegate.swift
+++ 3LED/AppDelegate.swift
@@ -7,18 +7,24 @@
 //
 
 import UIKit
-import LIFXClient
 import Network
+import LIFXClient
+import PromiseKit
 
 @UIApplicationMain class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-    var client = LIFXClient(address: IPv4Address("192.168.1.83")!, source: 1234)
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        client.connect()
+        LIFXClient.connect(host: .ipv4(IPv4Address("192.168.1.83")!), source: UInt32(1337)).then { client -> Promise<LIFXPacket<Light.State>> in
+            let message = Light.SetColor(color: Light.HSBK(hue: 0x0000, saturation: 0xFFFF, brightness: 0xFFFF, kelvin: 3500), duration: 1000)
+            let packet = LIFXPacket(source: client.source, response: true, message: message)
+            return client.request(packet)
+        }.done { response in
+            print(response)
+        }.catch { error in
+            print(error)
+        }
         
         return true
     }
